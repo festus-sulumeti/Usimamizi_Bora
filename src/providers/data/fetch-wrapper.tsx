aab23018a1a013c47676fc51c6bb8174d2e0c618
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-01 16:02:51 +0300

    The project

--- src/providers/data/fetch-wrapper.tsx
+++ src/providers/data/fetch-wrapper.tsx
@@ -1,5 +1,34 @@
+import { GraphQLFormattedError } from "graphql";
+
 const customFetch = async (url: string, options: RequestInit) => {
     const accessToken = localStorage.getItem('access_token');
 
-    const headers = options.headers || {};
+    const headers = options.headers as Record<string, string>;
+
+    return await fetch(url, {
+
+        ...options,
+        headers: {
+            ...headers,
+            Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+            "content-type": "application/json",
+            "Appollo-Require-Preflight": "true",
+        },
+    })
+}
+
+const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>): Error | null => {
+    if(!body){
+        return {
+            message: 'unknown error',
+            statusCode: "INTERNAL_SERVER_ERROR"
+        }
+    }
+
+    if("errors" in body){
+        const errors = body?.errors;
+
+        const messages = errors?.map((error) => error?.message)?.join("");
+        const code = errors?.[0]?.extensions?.code;
+    }
 }
